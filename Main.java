3b1a9dd9bf46746574e945467006bd35f7ecf732
Yalam Sunil <123179590+SunilYalam@users.noreply.github.com>
2023-02-02 18:23:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+public class Main{
+    public static void main(String args[]){
+        int x = 2;
+        int y = 4;
+        int product = x*y;
+        System.out.println(product);
+
+        char ch = 'U';
+        int ascii = ch;
+        System.out.println(ch + ascii);
+
+        // Rectangle area
+        int l = 7;
+        int b = 4;
+        int rect = l*b;
+        System.out.println("area of rectangle is ="+ rect);
+
+        // calculate of the cube
+        int A = 4;
+        int cube = A*A*A;
+        System.out.println("calculate of the cube ="+ cube);
+
+        // Swap two numbers
+       int X = 10;
+       int Y = 6;
+       X = X + Y;
+       Y = Y - X;
+       X = X - Y;
+       System.out.println(X + Y);
+    }
+}
